2ba42b7790c0c0842a6b83dd9d128f18e2cf3955
may-may123 <thitsaraung471@gmail.com>
2025-12-23 21:29:27 +0100

    christmas

---
+++ script.js
@@ -0,0 +1,7 @@
+function openGift() {
+    document.getElementById("giftModal").style.display = "block";
+}
+
+function closeGift() {
+    document.getElementById("giftModal").style.display = "none";
+}
